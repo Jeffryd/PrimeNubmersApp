967915b0a1945e23f195165ad5c9b7355d6dfff2
Jeffryd <1130177@unav.edu.mx>
2021-05-18 13:16:17 -0700

    pascalize words

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,11 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { PrimosComponent } from './primos/primos.component';
 import { BrujulaComponent } from './brujula/brujula.component';
+import { PascalizeWordsComponent } from './pascalize-words/pascalize-words.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, PrimosComponent, BrujulaComponent ],
+  declarations: [ AppComponent, HelloComponent, PrimosComponent, BrujulaComponent, PascalizeWordsComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/pascalize-words/pascalize-words.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pascalize-words',
+  templateUrl: './pascalize-words.component.html',
+  styleUrls: ['./pascalize-words.component.css']
+})
+export class PascalizeWordsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+    let oracion = "i like javascript";
+
+    console.log( this.pascalize( oracion ) );
+  }
+
+  pascalize( words: string ){
+      return words.match(/[a-zA-Z0-9]+/g)
+      .map( word => {
+        return word[0].toUpperCase() + word.slice(1).toLowerCase()
+      }).join("")
+  }
+
+}
