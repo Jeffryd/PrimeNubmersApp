7ab830fe0f7eaaf4e737dfe248b13402637023a0
Jeffryd <1130177@unav.edu.mx>
2021-05-19 06:58:55 -0700

    fizz buzz terminado

--- src/app/fizzbuzz/fizzbuzz.component.ts
+++ src/app/fizzbuzz/fizzbuzz.component.ts
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FizzbuzzComponent implements OnInit {
 
+  numbers: number[];
+
   constructor() { }
 
   ngOnInit() {
+    this.numbers = this.range( 1, 100, 1 );
+    console.log( this.numbers )
+  }
+
+  range(start: number, stop: number, step: number) : number[] {
+    let a = [start] , b = start;
+    while( b < stop ){
+      b += step;
+      a.push(b);
+    }
+    return a;
   }
 
 }
